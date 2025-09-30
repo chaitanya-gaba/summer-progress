8443b1295358be630363b328914cc33fe9855877
chaitanya-gaba <chaitanya.gabadav@gmail.com>
2025-09-30 16:35:47 +0530

    index for hosted pages

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('redirectBtn').addEventListener('click', function() {
+    // Replace this URL with the folder or page where your CSS designs live
+    window.location.href = 'file1/sub-file1/';  
+});
