27d680d6251d192850b009413346cba2a9fba296
chaitanya-gaba <chaitanya.gabadav@gmail.com>
2025-09-30 16:51:04 +0530

    added repo-link

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById('redirectBtn').addEventListener('click', function() {
     // Replace this URL with the folder or page where your CSS designs live
-    window.location.href = 'file1/sub-file1/';  
+    window.location.href = 'https://github.com/chaitanya-gaba/summer-progress/tree/main/front_end';  
 });
