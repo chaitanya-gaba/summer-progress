b73fe8638e6c1a24eed8dc81d5db01ce0fd9ca6f
chaitanya-gaba <chaitanya.gabadav@gmail.com>
2025-10-23 21:49:33 +0530

    update-design

--- script.js
+++ script.js
@@ -33,7 +33,8 @@ const designs = [
   "front_end/gemini_Test/cheer-up",
   "front_end/gemini_Test/landing-page",
   "front_end/gemini_Test/profile-card",
-  "front_end/gemini_Test/diwali-greetings"
+  "front_end/gemini_Test/diwali-greetings",
+  "front_end/gemini_Test/letter-L"
   // ... add all design folder names here
 ];
 
