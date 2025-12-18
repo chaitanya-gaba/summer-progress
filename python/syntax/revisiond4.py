ff5a74fa097b562c8005aca32c9d4c204574096f
chaitanya-gaba <chaitanya.gabadav@gmail.com>
2025-12-18 12:56:08 +0530

    bug-fix

--- python/syntax/revisiond4.py
+++ python/syntax/revisiond4.py
@@ -117,11 +117,27 @@ for brand, details in volkswogen.items():
         details["year"],
         details["most_popular"]
     )
-# for details in volkswogen.values():
-#     print(details["country"])
-# if "audi" in volkswogen:
-#     print("Audi exists")
-# if "country" in volkswogen["audi"]:
-#     print("Country info available")
-# volkswogen["audi"]["most_popular"] = "Q5"
-# volkswogen["audi"]["ceo"] = "Gernot Döllner"
+for details in volkswogen.values():
+    print(details["country"])
+if "audi" in volkswogen:
+    print("Audi exists")
+if "country" in volkswogen["audi"]:
+    print("Country info available")
+volkswogen["audi"]["most_popular"] = "Q5"
+volkswogen["audi"]["ceo"] = "Gernot Döllner"
+print(volkswogen)
+
+'''
+Method	Description
+clear()	Removes all the elements from the dictionary
+copy()	Returns a copy of the dictionary
+fromkeys()	Returns a dictionary with the specified keys and value
+get()	Returns the value of the specified key
+items()	Returns a list containing a tuple for each key value pair
+keys()	Returns a list containing the dictionary's keys
+pop()	Removes the element with the specified key
+popitem()	Removes the last inserted key-value pair
+setdefault()	Returns the value of the specified key. If the key does not exist: insert the key, with the specified value
+update()	Updates the dictionary with the specified key-value pairs
+values()	Returns a list of all the values in the dictionary
+'''
