f5b3368f520afbea706c94cc7364c34db7364d81
chaitanya-gaba <chaitanya.gabadav@gmail.com>
2025-12-23 15:55:22 +0530

    arguments

--- python/syntax/revisiond8.py
+++ python/syntax/revisiond8.py
@@ -37,6 +37,18 @@ print(variable_name(5, 6))
 variable_name = lambda input_variable1, input_variable2, input_variable3 : input_variable1 * input_variable2 ** input_variable3
 print(variable_name(2, 4, 6))
 
+numbers = [1, 2, 3, 4, 5]
+doubled = list(map(lambda x: x * 2, numbers))
+print(doubled)
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+odd_numbers = list(filter(lambda x: x % 2 != 0, numbers))
+print(odd_numbers)
+
+students = [("Emil", 25), ("Tobias", 22), ("Linus", 28)]
+sorted_students = sorted(students, key=lambda x: x[1])
+print(sorted_students)
+
 # decorators
 def changecase(func):
 
@@ -124,3 +136,47 @@ def myfunction():
   return "Tobias"
 
 print(myfunction())
+
+
+import time
+
+def timer_func(func):
+  def wrapper(*args):
+    start = time.time()
+    print(f"Start: {start}")
+    func(*args)
+    end = time.time()
+    print(f"End: {end}")
+    return f"{func.__name__} ran in {end - start}"
+  return wrapper
+
+@timer_func
+def delay(n):
+  time.sleep(n)
+
+x = delay(0)
+print(x)
+
+def name_printer(func):
+  def inner():
+    print(f"Name of the function is: {func.__name__}")
+    func()
+  return inner
+
+@name_printer
+def greet():
+  print("Hello")
+
+greet()
+
+def deco(func):
+  def inner(*args, **kwargs):
+    print(f"Calling Function: {func.__name__}")
+    func(*args, **kwargs)
+  return inner
+
+@deco
+def greet(name, age):
+  print(f"Hello {name}, you are {age} years old!")
+
+greet("Ojas", 21)
